930da71e159041ab216c4c4b269a5ddfe9e25dde
calin latcovschi <clatcovschi@gmail.com>
2023-01-04 17:30:08 +0200

    add environments

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  api_url: 'https://api.pistrix.md',
+  google_metrics: 'G-8ZD68FSPBR',
+  google_cloud_file_url: 'https://storage.googleapis.com/pistrix_technologies/'
+};
