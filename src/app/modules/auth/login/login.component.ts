776b8f9bfa14fd550e3874b9f83cb93e4081f096
calin latcovschi <clatcovschi@gmail.com>
2023-01-04 19:06:16 +0200

    add custom login

---
+++ src/app/modules/auth/login/login.component.ts
@@ -0,0 +1,29 @@
+import {Component} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {AuthService} from "../auth.service";
+
+@Component({
+  selector: 'login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder,
+              private authService: AuthService) {
+
+    this.form = this.fb.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  login() {
+    const val = this.form.value;
+
+    if (val.email && val.password) {
+      this.authService.login(val.email, val.password);
+    }
+  }
+}
